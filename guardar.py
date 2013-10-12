73f2c38d8c1e25c6b8cd835ac33eab5b3e0ad5d4
valentin <valentin@localhost.localdomain>
2013-10-12 18:08:46 -0300

    version 1.0.3 de icaro

--- guardar.py
+++ guardar.py
@@ -55,178 +55,25 @@ def guardar(obj,ruta,fondo):
             pickle.dump(fondo.objetos[i].pegado_a,file)
             pickle.dump(fondo.objetos[i+1].posicion,file)
             pickle.dump(fondo.objetos[i+1].pegado_a,file)
-
+    file.close()
     return 0
 
+# esto todavia falta de implementar
+#        if fondo.tipo_obj[i]==4:
+#            file.writelines("<objeto_cero>")
+#            file.write("\n")
+#            file.writelines(str(fondo.objetos[i].ide))
+#            file.write("\n")
+#            file.writelines(str(fondo.objetos[i].posicion))
+#            file.write("\n")
+#            file.writelines(str(fondo.objetos[i].posicion2))
+#            file.write("\n")
+#            file.writelines(str(fondo.objetos[i].color))
+#            file.write("\n")
+#            file.writelines(str(fondo.objetos[i].texto))
+#            file.write("\n")
+#            file.writelines("</objeto_cero>")
+#            file.write("\n")
 
 
-
-
-
-            
-            
-            
-            
-
-    print "-------------------"
-    print "tamaño obj datos",len(fondo.tipo_obj_datos)
-    print "datos",fondo.objetos_datos
-
-    print "tamaño obj",len(fondo.tipo_obj)
-    print "obj",fondo.objetos
-    print "-------------------"
-    file.writelines("<fondo>")
-    file.write("\n")
-    file.writelines(str(fondo.FONDO))
-    file.write("\n")
-    file.writelines(str(fondo.band))
-    file.write("\n")
-    file.writelines(str(fondo.img))
-    file.write("\n")
-    file.writelines("</fondo>")
-    file.write("\n")
-    file.writelines("<objeto_inicial>")
-    file.write("\n")
-    file.writelines(str(fondo.objetos[0].posicion))
-    file.write("\n")
-
-    file.writelines("</objeto_inicial>")
-    file.write("\n")
-
-    for i in range(len(fondo.objetos_datos)):
-        print "este es el tamañno del obj datos",len(fondo.tipo_obj_datos)
-        if fondo.tipo_obj_datos[i]==7:
-            file.writelines("<objeto_dato>")
-            file.write("\n")
-            file.writelines(str(fondo.objetos_datos[i].ide))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].posicion))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].texto))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].color))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].modificable))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].valor_cadena_no_mod))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].cadena_intermedia))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].cadena_final))
-            file.write("\n")
-
-            file.writelines("</objeto_dato>")
-            file.write("\n")
-
-        if fondo.tipo_obj_datos[i]==6:
-            file.writelines("<objeto_dato_img>")
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].imagenintermedia))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].ide))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].posicion))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].texto))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].color))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].modificable))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].valor_cadena_no_mod))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].cadena_intermedia))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos_datos[i].cadena_final))
-            file.write("\n")
-
-            file.writelines("</objeto_dato_img>")
-            file.write("\n")
-    for i in range(len(fondo.tipo_obj)):
-        print fondo.tipo_obj
-        print i
-
-        if fondo.tipo_obj[i]==4:
-            file.writelines("<objeto_cero>")
-            file.write("\n")
-
-
-            file.writelines(str(fondo.objetos[i].ide))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos[i].posicion))
-            file.write("\n")
-            file.writelines(str(fondo.objetos[i].posicion2))
-            file.write("\n")
-            file.writelines(str(fondo.objetos[i].color))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos[i].texto))
-            file.write("\n")
-
-            file.writelines("</objeto_cero>")
-            file.write("\n")
-
-        if fondo.tipo_obj[i]==5:
-            file.writelines("<objeto_bloque>")
-            file.write("\n")
-
-
-            file.writelines(str(fondo.objetos[i].ide))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos[i].posicion))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos[i].color))
-            file.write("\n")
-            file.writelines(str(fondo.objetos[i].texto))
-            file.write("\n")
-            file.writelines(str(fondo.objetos[i+1].posicion))
-            file.write("\n")
-            file.writelines("</objeto_bloque>")
-            file.write("\n")
-        if fondo.tipo_obj[i]==1:
-            print " error:" , i
-            file.writelines("<objeto_componente>")
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos[i].imagenintermedia))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos[i].arg))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos[i].ide))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos[i].posicion))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos[i].color))
-            file.write("\n")
-
-            file.writelines(str(fondo.objetos[i].texto))
-            file.write("\n")
-
-            file.writelines("</objeto_componente>")
-            file.write("\n")
-
-    file.close()
 
