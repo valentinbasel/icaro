88fa618375b3fb241543769e0fb18faf6338f5c4
valentin basel <valentin@localhost.localdomain>
2014-06-27 17:50:04 -0300

    componentes repetir

---
+++ componentes/core/06-estructuras_de_control/repetir/bloque.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  bloque.py
+#
+#  Copyright 2013 valentin <valentinbasel@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+
+class CMP:
+
+    def __init__(self):
+
+        self.valor = [
+            "for(_i=0;_i<",
+            "valor",
+            ";_i++){"
+        ]
+        self.dicc = {
+            "nombre": "repetir",
+            "componente": 5,
+            "cant_puertos": 0,
+            "color": (10, 30, 90),
+            "dato": "fin ",
+                    "dato2": "0",
+                    "valor": self.valor
+        }
+        self.tootips = "Repite las instrucciones dentro del bloque "
+        self.img = ""
