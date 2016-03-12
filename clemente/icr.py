908460caddb6daf6defc90347702200034a5bd21
vbasel <vbasel@localhost.localdomain>
2016-03-12 18:06:15 -0300

    modificaciones en clemente para que pueda leer CDC

--- clemente/icr.py
+++ clemente/icr.py
@@ -26,16 +26,48 @@ import time
 import threading
 import usb
 import random
+import apicaro
+
+class CDC(apicaro.puerto):
+    def __init__(self , servidor):
+        self.servidor=servidor
+        self.band = False
+        self.flag = True
+        self.status = False
+        #self.PUERTO="/dev/ttyACM1"
+
+    def conexion(self):
+        if self.servidor.emular == True:
+            return True
+        band=self.iniciar()
+        #print band
+        return band
+
+    def leer_dat(self):
+        
+        try:
+            for a in range(8):
+                n=a+1
+                valor = self.leer_analogico(a)
+                print "sens ", n , " = ", valor, " en sector ", a
+                time.sleep(0.1)
+                if valor <> False:
+                    self.servidor.datos[a]  = valor
+                    self.band = True
+                    self.status = True
+                else: 
+                    self.servidor.datos[a]  = 0
+
+        except:
+            print "error con el hardware icaro"
+            self.band = False
+            self.status = False
 
 
-class ICR(threading.Thread):
+class BULK(object):
 
-    """ Clase para manejar  """
-
-    def __init__(self, servidor):
-        """ Class initialiser """
-        self.servidor = servidor
-        threading.Thread.__init__(self)
+    def __init__(self,servidor):
+        self.servidor=servidor
         self.band = False
         self.flag = True
         self.status = False
@@ -62,38 +94,57 @@ class ICR(threading.Thread):
         except Exception, ex:
             return False
 
+    def leer_dat(self):
+        try:
+            cadena = ""
+            for i in self.dh.bulkRead(0x82, 39, 10000):
+                cadena += chr(i)
+            cad_aux = cadena.split("\n")
+            cad_fin = cad_aux[0].split(",")
+            if len(cad_fin) >= 8:
+                for a in range(8):
+                    self.servidor.datos[a] = cad_fin[a]
+            self.band = True
+            self.status = True
+            #~ print self.servidor.datos
+        except Exception, ex:
+            print "error con el hardware icaro"
+            self.band = False
+            self.status = False
+
+
+class ICR(threading.Thread):
+
+    """ Clase para manejar  """
+
+    def __init__(self, servidor):
+        """ Class initialiser """
+        self.servidor = servidor
+        threading.Thread.__init__(self)
+        self.band = False
+        self.flag = True
+        self.status = False
+        self.protocolo_con=CDC(servidor)
+
     def actualizar(self, band):
 
         if self.servidor.emular == True:
+
             self.status = True
             for a in range(8):
                 self.servidor.datos[a] = random.randint(0, 1023)
             return True
-
+        self.status = self.protocolo_con.status
         if self.band == True:
-            try:
-                cadena = ""
-                for i in self.dh.bulkRead(0x82, 39, 10000):
-                    cadena += chr(i)
-                cad_aux = cadena.split("\n")
-                cad_fin = cad_aux[0].split(",")
-                if len(cad_fin) >= 8:
-                    for a in range(8):
-                        self.servidor.datos[a] = cad_fin[a]
-                self.band = True
-                self.status = True
-                #~ print self.servidor.datos
-            except Exception, ex:
-                print "error con el hardware icaro"
-                self.band = False
-                self.status = False
+            self.protocolo_con.leer_dat()
         return self.band
+
 
     def stop(self):
         self.flag = False
 
     def run(self):
-        self.band = self.conexion()
+        self.band = self.protocolo_con.conexion()
         #~ print "estado de la placa icaro : ", self.band
         while self.flag == True:
             self.band = self.actualizar(self.band)
@@ -102,7 +153,7 @@ class ICR(threading.Thread):
                 #"la placa se desactivo"
                 while self.band == False:
                     # print "reconectando"
-                    self.band = self.conexion()
+                    self.band = self.protocolo_con.conexion()
                     time.sleep(3)
                     if self.band == True:
                         print "se volvio a conectar la placa"
